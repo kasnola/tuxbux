18c83dc95baa38c1434dbf0e9925f744251e5245
kasnola <157547102+kasnola@users.noreply.github.com>
2025-03-16 18:23:28 -0500

    i like this outer border on the shop entries better

--- tuxbux.py
+++ tuxbux.py
@@ -2,6 +2,7 @@ from time import monotonic
 
 from textual import log
 from textual.app import App, ComposeResult
+from textual.content import Content
 from textual.containers import Middle, HorizontalGroup, VerticalGroup, VerticalScroll, Center
 from textual.reactive import reactive
 from textual.widgets import Button, Digits, Footer, Header, Static, Label
@@ -11,28 +12,34 @@ class Shop(VerticalScroll):
 
 	def compose(self) -> ComposeResult:
 		"""Create child purchasables."""
-		yield Purchasable(name="Product 00", classes="purchasable")
-		yield Purchasable(name="Product 01", classes="purchasable")
-		yield Purchasable(name="Product 02", classes="purchasable")
-		yield Purchasable(name="Product 03", classes="purchasable")
-		yield Purchasable(name="Product 04", classes="purchasable")
-		yield Purchasable(name="Product 05", classes="purchasable")
+		yield Purchasable(name="Product 00", classes="purchasable", id="tuxminer")
+		yield Purchasable(name="Product 01", classes="purchasable", id="tuxminer2")
+		yield Purchasable(name="Product 02", classes="purchasable", id="tuxminer3")
+		yield Purchasable(name="Product 03", classes="purchasable", id="tuxminer4")
+		yield Purchasable(name="Product 04", classes="purchasable", id="tuxminer5")
+		yield Purchasable(name="Product 05", classes="purchasable", id="tuxminer6")
 
 class Purchasable(HorizontalGroup):
 	"""A purchasable widget."""
 
+	def on_mount(self) -> None:
+		self.border_title = self.name
+
 	def on_button_pressed(self, event: Button.Pressed) -> None:
 		"""Event handler called when a button is pressed."""
 		button_id = event.button.id
-		self.log("button pressed!")
+		self.log("button pressed @ " + str(button_id))
 
 	def compose(self) -> ComposeResult:
 		"""Create child widgets of a purchasable."""
 		yield VerticalGroup(
-			Label("[bold]TuxMiner[/bold]", id="tuxminer"),
+			Label(
+				Content.from_markup("[bold]$name[/bold]", name=self.name),
+				id=self.id
+				),
 			classes="label"
 		)
-		yield Middle(Button("Buy", classes="button buy"))
+		yield Middle(Button("Buy", classes="button buy", id=self.id))
 
 class TuxbuxIdleGameApp(App):
 	"""An idle game made with Textual."""
